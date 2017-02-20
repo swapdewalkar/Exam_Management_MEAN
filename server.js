5cab8f404a9b8d3cdc392dc55d14e6d9b24cc1b0
swapdewalkar <swapdewalkar@gmail.com>
2017-02-20 22:21:34 +0530

    Passport Login for Express Used Session and Cookie  Changes to be committed: 	new file:   models/account.js 	modified:   package.json 	modified:   routes/index.js 	modified:   server.js 	new file:   views/home.html 	new file:   views/login.html 	new file:   views/register.html

--- server.js
+++ server.js
@@ -4,24 +4,41 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var tasks = require('./routes/tasks');
 var dbhandle = require('./routes/dbhandle');
-
-var port=4000;
+var methodOverride = require('method-override');
+var session = require('express-session');
+var http = require('http');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var cookieParser = require('cookie-parser');
 
 var app=express();
-
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs');
 app.engine('html',require('ejs').renderFile);
 
+app.use(methodOverride());
+app.use(session({ resave: true, saveUninitialized: true,
+                  secret: 'uwotm8' }));
+app.use(cookieParser("swapnil"));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname,'client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+var port=4000;
+
+//var db =mongoose.connect('mongodb://localhost/loginapp');
+var Account = require('./models/account');
+//passport.use(Account.createStrategy());
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 app.use('/',index);
 app.use('/api',tasks);
 app.use('/db',dbhandle);
 app.all('/*', function(req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
     res.render('index.html');
 });
 
